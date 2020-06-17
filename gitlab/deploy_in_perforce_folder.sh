dfe705961df3dd8e40184c76898976f48721b54a
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2020-06-17 20:32:29 +0200

    ci: refactor cicd script and add deploy jobs

---
+++ gitlab/deploy_in_perforce_folder.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# This script assume $CI_ENVIRONMENT_URL contains a perforce folder path after http://
+# For example http://RRSpecial/_Tech/Blender/addons would map to //RRSpecial/_Tech/Blender/addons
+# Perforce access is made with client workspace stored in P4CLIENT
+
+if [ -z $P4CLIENT ]; then
+  echo "Specify P4CLIENT env var: it should contain a perforce client name that can be used on the runner."
+  exit 1
+fi
+
+if [ -z $1 ]; then
+  echo "Usage: $0 <zipfile>"
+  exit 1
+fi
+
+OFFSET=`echo "http:" | wc -c`
+PERFORCE_PATH=`echo $CI_ENVIRONMENT_URL | cut -c $OFFSET-`
+
+echo "PERFORCE_PATH = $PERFORCE_PATH"
+
+PATHS=`p4 where $PERFORCE_PATH | tr '\' '/'`
+IFS=' '
+read -a strarr <<< "$PATHS"
+
+DEPLOY_PATH=${strarr[-1]}
+
+echo "Local path is $DEPLOY_PATH"
+
+if [ ! -e $DEPLOY_PATH ]; then
+  echo "Error: Deploy path does not exists, create it first."
+  exit 1
+fi
+
+p4 sync
+
+if [ -e $DEPLOY_PATH/mixer ]; then
+  echo "Deleting previous mixer installation"
+  shopt -s globstar dotglob
+  pushd $DEPLOY_PATH/mixer/
+  for file in **/*; do
+    p4 delete "$PERFORCE_PATH/mixer/$file"
+  done
+  popd
+  rm -rf $DEPLOY_PATH/mixer
+fi
+p4 submit -d "Remove old mixer installation"
+p4 sync
+
+echo "Unzipping mixer installation"
+unzip $1 -d $DEPLOY_PATH
+
+if [ -e $DEPLOY_PATH/mixer ]; then
+  echo "Adding new mixer installation"
+  shopt -s globstar dotglob
+  pushd $DEPLOY_PATH/mixer/
+  for file in **/*; do
+    p4 add "$PERFORCE_PATH/mixer/$file"
+  done
+  popd
+fi
+
+p4 submit -d "Update mixer to $CI_COMMIT_REF_NAME"
+p4 sync
