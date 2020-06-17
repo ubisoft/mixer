741209c2de09c7bde62662f027b8f905f526c502
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2020-06-17 11:35:33 +0200

    ci: use a test path for deploy

--- gitlab/deploy_network_common.sh
+++ gitlab/deploy_network_common.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 DOWNLOAD_URL=$CI_PROJECT_URL/releases/$CI_COMMIT_REF_NAME/downloads/mixer-addon-zip
-DEPLOY_PATH=//ubisoft.org/mtrstudio/World/UAS/Tech/_DEPLOY/blender/addons_intern/common
+DEPLOY_PATH=//ubisoft.org/mtrstudio/World/UAS/Tech/_DEPLOY/blender/addons_intern/_deploy_test
+
+mkdir -p $DEPLOY_PATH
 
 curl $DOWNLOAD_URL > $DEPLOY_PATH/mixer-$CI_COMMIT_REF_NAME.zip
 rm -rf $DEPLOY_PATH/mixer
