16f27ad1d8ed7ddb44ce2ac99988ef369a6afb5d
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2020-06-17 14:15:29 +0200

    ci: simplify deploy by using an artifact

--- gitlab/deploy_network_common.sh
+++ gitlab/deploy_network_common.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-DOWNLOAD_URL=$CI_PROJECT_URL/releases/$CI_COMMIT_REF_NAME/downloads/mixer-addon-zip
 DEPLOY_PATH=//ubisoft.org/mtrstudio/World/UAS/Tech/_DEPLOY/blender/addons_intern/_deploy_test
 
 mkdir -p $DEPLOY_PATH
 
-curl $DOWNLOAD_URL > $DEPLOY_PATH/mixer-$CI_COMMIT_REF_NAME.zip
-rm -rf $DEPLOY_PATH/mixer
-unzip $DEPLOY_PATH/mixer-$CI_COMMIT_REF_NAME.zip
+unzip $1 $DEPLOY_PATH/
 # rm mixer-$CI_COMMIT_REF_NAME.zip
