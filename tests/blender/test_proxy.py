a07974ba9d24aed983a3f2a9f5309959b2330c0a
Philippe Crassous <philippe.crassous@ubisoft.com>
2020-07-06 14:30:19 +0200

    test: duplicate uuid

--- tests/blender/test_proxy.py
+++ tests/blender/test_proxy.py
@@ -3,17 +3,24 @@ from tests.blender.blender_testcase import TestGenericJoinBefore
 
 
 class TestBpyProxy(TestGenericJoinBefore):
+    def force_sync(self):
+        action = f"""
+import bpy
+bpy.data.scenes[0].use_gravity = not bpy.data.scenes[0].use_gravity
+"""
+        self.send_string(action)
+
     def test_just_join(self):
 
         self.end_test()
 
-    def test_duplicate_uuid(self):
+    def test_duplicate_uuid_metaball(self):
+        # with metaballs the effect of duplicate uuids is visible as they are not
+        # handled by the VRtist protocol
         action = f"""
 import bpy
-bpy.ops.object.light_add(type='AREA')
-light = bpy.context.active_object
-light.name = "light0"
-light.shape = "ELLIPSE"
+bpy.ops.object.metaball_add(type='BALL', location=(0,0,0))
+obj = bpy.context.active_object
 """
         self.send_string(action)
 
@@ -21,10 +28,11 @@ light.shape = "ELLIPSE"
 import bpy
 D = bpy.data
 bpy.ops.object.duplicate()
-bpy.context.active_object.name = "light1"
-assert D.objects["light0"].mixer_uuid == D.objects["light1"].mixer_uuid
+bpy.ops.transform.translate(value=(0, 4, 0))
 """
         self.send_string(action)
+
+        self.force_sync()
 
         self.end_test()
 
