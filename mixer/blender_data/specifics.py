ubisoft/mixer

commit 816d24d5b49700f60fd2b9e42392a8365d3eb36d

fix: failed bpy_prop_collection.add()

mixer/blender_data/specifics.py
@@ -218,6 +218,16 @@ effect_sequences = set(T.EffectSequence.bl_rna.properties["type"].enum_items.key
 def add_element(proxy: Proxy, collection: T.bpy_prop_collection, key: str):
     """Add an element to a bpy_prop_collection using the collection specific API
     """
+
+    if isinstance(collection, T.bpy_prop_collection):
+        try:
+            return collection.add()
+        except Exception:
+            logger.warning(f"Failed collection.add() for type {type(collection)}, value{collection} ...")
+            for s in traceback.format_exc().splitlines():
+                logger.warning(f"...{s}")
+            return None
+
     bl_rna = getattr(collection, "bl_rna", None)
     if bl_rna is not None:
         if isinstance(bl_rna, type(T.KeyingSets.bl_rna)):

commit 05f047215179ddcea35a661abab90a5f15c07a7a

fix: regression from 816d24d5b49700f60fd2b9e42392a8365d3eb36d

mixer/blender_data/specifics.py
@@ -219,15 +219,6 @@ def add_element(proxy: Proxy, collection: T.bpy_prop_collection, key: str):
     """Add an element to a bpy_prop_collection using the collection specific API
     """
 
-    if isinstance(collection, T.bpy_prop_collection):
-        try:
-            return collection.add()
-        except Exception:
-            logger.warning(f"Failed collection.add() for type {type(collection)}, value{collection} ...")
-            for s in traceback.format_exc().splitlines():
-                logger.warning(f"...{s}")
-            return None
-
     bl_rna = getattr(collection, "bl_rna", None)
     if bl_rna is not None:
         if isinstance(bl_rna, type(T.KeyingSets.bl_rna)):
@@ -291,6 +282,11 @@ def add_element(proxy: Proxy, collection: T.bpy_prop_collection, key: str):
             name = proxy.data("name")
             type_ = proxy.data("type")
             return collection.new(name, type_)
+
+    try:
+        return collection.add()
+    except Exception:
+        pass
 
     # try our best
     new_or_add = getattr(collection, "new", None)