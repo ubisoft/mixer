b94e4f5b1ec56b35fe393a3c4fbd4b315b8ffdbb
Philippe Crassous <philippe.crassous@ubisoft.com>
2020-08-04 17:42:18 +0200

    refactor: move logging utilities into log_utils.py

--- mixer/__init__.py
+++ mixer/__init__.py
@@ -7,7 +7,6 @@ Register/unregister functions and logging setup.
 import atexit
 import faulthandler
 import logging
-import os
 from pathlib import Path
 
 bl_info = {
@@ -47,53 +46,12 @@ def cleanup():
         faulthandler.disable()
 
 
-class Formatter(logging.Formatter):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def format(self, record: logging.LogRecord):
-        """
-        The role of this custom formatter is:
-        - append filepath and lineno to logging format but shorten path to files, to make logs more clear
-        - to append "./" at the begining to permit going to the line quickly with VS Code CTRL+click from terminal
-        """
-        s = super().format(record)
-        pathname = Path(record.pathname).relative_to(MODULE_PATH)
-        s += f" [{os.curdir}{os.sep}{pathname}:{record.lineno}]"
-        return s
-
-
-def get_logs_directory():
-    def _get_logs_directory():
-        import tempfile
-
-        if "MIXER_USER_LOGS_DIR" in os.environ:
-            username = os.getlogin()
-            base_shared_path = Path(os.environ["MIXER_USER_LOGS_DIR"])
-            if os.path.exists(base_shared_path):
-                return os.path.join(os.fspath(base_shared_path), username)
-            logger.error(
-                f"MIXER_USER_LOGS_DIR env var set to {base_shared_path}, but directory does not exists. Falling back to default location."
-            )
-        return os.path.join(os.fspath(tempfile.gettempdir()), "mixer")
-
-    dir = _get_logs_directory()
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-    return dir
-
-
-def get_log_file():
-    from mixer.share_data import share_data
-
-    return os.path.join(get_logs_directory(), f"mixer_logs_{share_data.run_id}.log")
-
-
 def register():
     from mixer import bl_panels
     from mixer import bl_operators
     from mixer import bl_properties, bl_preferences
     from mixer.blender_data import debug_addon
+    from mixer.log_utils import Formatter, get_log_file
 
     if len(logger.handlers) == 0:
         logger.setLevel(logging.WARNING)
