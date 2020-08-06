63765db7880e4eb7ccb6e3dde0a0986300b26bef
Philippe Crassous <philippe.crassous@ubisoft.com>
2020-08-06 11:47:01 +0200

    fix: build packed_file image

--- mixer/blender_data/specifics.py
+++ mixer/blender_data/specifics.py
@@ -24,9 +24,7 @@ def bpy_data_ctor(collection_name: str, proxy: BpyIDProxy) -> Union[T.ID, None]:
         image = None
         if is_packed:
             name = proxy.data("name")
-            size = proxy.data("size")
-            width = size.data(0)
-            height = size.data(1)
+            width, height = proxy.data("size")
             image = collection.new(name, width, height)
             # remaning attributes will be saved from the received proxy attributes
         else:
