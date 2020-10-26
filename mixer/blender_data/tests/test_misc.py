3c580bdac01351113eed7ec233b30b0eec331a25
Philippe Crassous <philippe.crassous@ubisoft.com>
2020-10-26 11:52:22 +0100

    test: fix for NonePtrProxy

--- mixer/blender_data/tests/test_misc.py
+++ mixer/blender_data/tests/test_misc.py
@@ -27,6 +27,7 @@ from mixer.blender_data.aos_soa_proxy import SoaElement
 from mixer.blender_data.blenddata import BlendData
 from mixer.blender_data.bpy_data_proxy import BpyDataProxy
 from mixer.blender_data.datablock_ref_proxy import DatablockRefProxy
+from mixer.blender_data.misc_proxies import NonePtrProxy
 from mixer.blender_data.filter import SynchronizedProperties, TypeFilterOut, test_properties, test_filter
 from mixer.blender_data.tests.utils import test_blend_file
 
@@ -151,7 +152,7 @@ class TestLoadProxy(unittest.TestCase):
         # load into proxy
         cam_proxy = self.proxy.data("cameras").search_one("Camera_0")
         focus_object_proxy = cam_proxy.data("dof").data("focus_object")
-        self.assertIs(focus_object_proxy, None)
+        self.assertIsInstance(focus_object_proxy, NonePtrProxy)
 
 
 class TestProperties(unittest.TestCase):
