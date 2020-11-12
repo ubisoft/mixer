674cd5895af871355d51e627813aaa47c3222f04
Philippe Crassous <philippe.crassous@ubisoft.com>
2020-11-12 18:53:37 +0100

    refactor: remove dead code

--- mixer/blender_data/debug_addon.py
+++ mixer/blender_data/debug_addon.py
@@ -33,17 +33,6 @@ class DebugDataProperties(bpy.types.PropertyGroup):
     test_names: bpy.props.StringProperty(name="TestNames", default=default_test)
 
 
-def timeit(func):
-    def wrapper(*arg, **kw):
-        """source: http://www.daniweb.com/code/snippet368.html"""
-        t1 = time.time()
-        res = func(*arg, **kw)
-        t2 = time.time()
-        return (t2 - t1), res, func.__name__
-
-    return wrapper
-
-
 proxy = None
 
 
