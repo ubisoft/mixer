12fc348031335712324df5ee946c78e2f7a889ea
Philippe Crassous <philippe.crassous@ubisoft.com>
2020-12-10 16:11:25 +0100

    test: workspaces draft

---
+++ tests/blender/test_workspace.py
@@ -0,0 +1,109 @@
+"""Tests for workspaces"""
+import unittest
+from parameterized import parameterized_class
+
+from tests import files_folder
+from tests.blender.blender_testcase import BlenderTestCase
+from tests.mixer_testcase import BlenderDesc
+
+
+class TestCase(BlenderTestCase):
+    def setUp(self):
+        sender_blendfile = files_folder() / "empty.blend"
+        receiver_blendfile = files_folder() / "empty.blend"
+        sender = BlenderDesc(load_file=sender_blendfile, wait_for_debugger=False)
+        receiver = BlenderDesc(load_file=receiver_blendfile, wait_for_debugger=False)
+        blenderdescs = [sender, receiver]
+        super().setUp(blenderdescs=blenderdescs)
+
+
+def _get_class_name(cls, num, params_dict):
+    # By default the generated class named includes either the "name"
+    # parameter (if present), or the first string value. This example shows
+    # multiple parameters being included in the generated class name:
+    return "%s_%s_%s" % (
+        cls.__name__,
+        num,
+        params_dict["name"],
+    )
+
+
+@parameterized_class(
+    [
+        {
+            "ws0": files_folder() / "workspaces" / "ws0_0",
+            "ws1": files_folder() / "workspaces" / "ws0_0",
+            "name": "Same",
+        },
+        {
+            "ws0": files_folder() / "workspaces" / "ws0_0",
+            "ws1": files_folder() / "workspaces" / "ws1_0",
+            "name": "Different",
+        },
+    ],
+    class_name_func=_get_class_name,
+)
+@unittest.skip("not implemented")
+class TestImageOneFolder(TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def setUp(self):
+        self.workspaces = [[str(self.ws0)], [str(self.ws1)]]
+        super().setUp()
+
+    def test_create_one_file(self):
+        path_a = str(self.ws0 / "image_a.png")
+        create = f"""
+import bpy
+bpy.data.images.load(r"{path_a}")
+"""
+        self.send_string(create)
+        self.end_test()
+
+    def test_create_two_files(self):
+        path_a = str(self.ws0 / "image_a.png")
+        path_b = str(self.ws0 / "image_b.png")
+        create = f"""
+import bpy
+bpy.data.images.load(r"{path_a}")
+bpy.data.images.load(r"{path_b}")
+"""
+        self.send_string(create)
+        self.end_test()
+
+
+@parameterized_class(
+    [
+        {
+            "ws0": [files_folder() / "workspaces" / "ws0_0", files_folder() / "workspaces" / "ws0_1"],
+            "ws1": [files_folder() / "workspaces" / "ws1_0", files_folder() / "workspaces" / "ws1_1"],
+            "name": "",
+        },
+    ],
+    class_name_func=_get_class_name,
+)
+@unittest.skip("not implemented")
+class TestImageTwoFolders(TestCase):
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+    def setUp(self):
+        self.workspaces = [[str(self.ws0)], [str(self.ws1)]]
+        super().setUp()
+
+    def test_create(self):
+        path_a = str(self.ws0[0] / "image_a.png")
+        path_c = str(self.ws0[1] / "image_c.png")
+        create = f"""
+import bpy
+bpy.data.images.load(r"{path_a}")
+bpy.data.images.load(r"{path_c}")
+"""
+        self.send_string(create)
+        self.end_test()
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/blender_app.py
+++ tests/blender_app.py
@@ -1,6 +1,6 @@
 import logging
 import time
-from typing import List, Optional, Mapping
+from typing import Iterable, List, Optional, Mapping
 import sys
 
 import tests.blender_lib as bl
@@ -32,7 +32,10 @@ _keep_room_open = """
 from mixer.share_data import share_data
 share_data.client.set_room_keep_open("{room_name}", {keep_room_open})
 """
-
+_set_workspace = """
+from mixer.tructruc import set_workspace
+set_workspace({workspace_folders})
+"""
 _join_room = """
 from mixer.connection import join_room
 from mixer.broadcaster.common import RoomAttributes
@@ -91,8 +94,17 @@ class BlenderApp:
 
         self._blender.send_string(_connect)
 
-    def create_room(self, room_name="mixer_unittest", keep_room_open=False, vrtist_protocol: bool = False):
+    def create_room(
+        self,
+        room_name="mixer_unittest",
+        keep_room_open=False,
+        vrtist_protocol: bool = False,
+        workspace_folders: List[str] = (),
+    ):
         """Emit a mixer create room command"""
+        if workspace_folders:
+            set_workspaces = _set_workspace.format(workspace_folders=list(workspace_folders))
+            self._blender.send_string(set_workspaces)
 
         create_room = _create_room.format(room_name=room_name, vrtist_protocol=vrtist_protocol)
         self._blender.send_string(create_room)
@@ -100,8 +112,16 @@ class BlenderApp:
         keep_room_open = _keep_room_open.format(room_name=room_name, keep_room_open=keep_room_open)
         self._blender.send_string(keep_room_open)
 
-    def join_room(self, room_name="mixer_unittest", vrtist_protocol: bool = False):
+    def join_room(
+        self,
+        room_name="mixer_unittest",
+        vrtist_protocol: bool = False,
+        workspace_folders: Iterable[str] = (),
+    ):
         """Emit a mixer join room command"""
+        if workspace_folders:
+            set_workspaces = _set_workspace.format(workspace_folders=list(workspace_folders))
+            self._blender.send_string(set_workspaces)
         join_room = _join_room.format(room_name=room_name, vrtist_protocol=vrtist_protocol, max_wait="{max_wait}")
         self._blender.send_string(join_room)
 
