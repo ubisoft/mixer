78a09f11875337d30b2dadfb356ac81bc36fd1e6
Philippe Crassous <philippe.crassous@ubisoft.com>
2021-02-26 11:58:57 +0100

    ci(tests): 2.92 release

--- gitlab/download_blender_builder.sh
+++ gitlab/download_blender_builder.sh
@@ -6,3 +6,4 @@ set -x
 CURRENT_DIR=`dirname $0`
 mkdir -p $CURRENT_DIR/blender/cache/
 curl https://builder.blender.org/download/$MIXER_BLENDER_ZIP_BASENAME.zip -o $CURRENT_DIR/blender/cache/$MIXER_BLENDER_ZIP_BASENAME.zip
+ls -l $CURRENT_DIR/blender/cache/$MIXER_BLENDER_ZIP_BASENAME.zip
--- gitlab/download_blender_release.sh
+++ gitlab/download_blender_release.sh
@@ -6,3 +6,4 @@ set -x
 CURRENT_DIR=`dirname $0`
 mkdir -p $CURRENT_DIR/blender/cache/
 curl https://download.blender.org/release/Blender$MIXER_BLENDER_VERSION_BASE/$MIXER_BLENDER_ZIP_BASENAME.zip -o $CURRENT_DIR/blender/cache/$MIXER_BLENDER_ZIP_BASENAME.zip
+ls -l $CURRENT_DIR/blender/cache/$MIXER_BLENDER_ZIP_BASENAME.zip
