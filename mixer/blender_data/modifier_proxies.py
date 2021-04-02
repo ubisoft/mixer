523f9eedd9190ef0851d16fa40d9e4a3e17d5db7
Philippe Crassous <philippe.crassous@ubisoft.com>
2021-04-02 17:06:34 +0200

    refactor: nodes modifier

--- mixer/blender_data/modifier_proxies.py
+++ mixer/blender_data/modifier_proxies.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-Utility proxy classes
+Proxy classes for bpy_struct that ned a custom implementation
 
 See synchronization.md
 """
@@ -40,6 +40,12 @@ logger = logging.getLogger(__name__)
 
 @serialize
 class NodesModifierProxy(StructProxy):
+    """Proxy for NodesModifier.
+
+    Requires special processing for the modifier inputs. They are "custom" properties with names that
+    match the node_group inputs identifiers like in e.g bpy.nodes.objects["Plane"].modifiers[0]["Input_5"].
+    """
+
     _serialize: Tuple[str, ...] = StructProxy._serialize + ("_inputs",)
 
     _non_inputs = set(("_RNA_UI",))
@@ -49,34 +55,38 @@ class NodesModifierProxy(StructProxy):
     def __init__(self):
         self._inputs: Dict[int, Any] = {}
         """{ index in geometry node group group input node : value }."""
+
         super().__init__()
 
-    def load_inputs(self, modifier: T.bpy_struct) -> Dict[int, Any]:
-        input_names = set(modifier.keys()) - self._non_inputs
-        geometry_node_group = modifier.node_group
-        if geometry_node_group is None:
-            return {}
-        geometry_node_group_inputs = {
-            tree_input.identifier: i for i, tree_input in enumerate(geometry_node_group.inputs)
-        }
-        return {geometry_node_group_inputs[input_name]: modifier.get(input_name) for input_name in input_names}
+    def _load_inputs(self, modifier: T.bpy_struct) -> Dict[int, Any]:
+        """Returns a dict containing {input_index_in_node_group_inputs: value_in_nodes_modifier} """
 
-    def save_inputs(self, modifier: T.bpy_struct):
-        geometry_node_group = modifier.node_group
-        if geometry_node_group is None:
+        input_names = set(modifier.keys()) - self._non_inputs
+        node_group = modifier.node_group
+        if node_group is None:
             return {}
-        geometry_node_group_inputs = {
-            i: tree_input.identifier for i, tree_input in enumerate(geometry_node_group.inputs)
-        }
+
+        input_indices = {tree_input.identifier: i for i, tree_input in enumerate(node_group.inputs)}
+
+        return {input_indices[name]: modifier.get(name) for name in input_names if name in input_indices}
+
+    def _save_inputs(self, modifier: T.bpy_struct):
+        """Saves the input values into modifier."""
+
+        node_group = modifier.node_group
+        if node_group is None:
+            return {}
+
+        inputs = {i: tree_input.identifier for i, tree_input in enumerate(node_group.inputs)}
         for input_index, value in self._inputs.items():
             # default serialization transforms int keys into string
-            input_name = geometry_node_group_inputs[int(input_index)]
+            input_name = inputs[int(input_index)]
             modifier[input_name] = value
 
     def load(self, modifier: T.bpy_struct, key: Union[int, str], context: Context) -> NodesModifierProxy:
         # The inputs are stored as "custom properties".
         # The keys are the geometry node_group group input node outputs.
-        self._inputs = self.load_inputs(modifier)
+        self._inputs = self._load_inputs(modifier)
         return super().load(modifier, key, context)
 
     def save(
@@ -99,12 +109,12 @@ class NodesModifierProxy(StructProxy):
 
         # update the geometry node reference before updating the input entries
         super().save(modifier, parent, key, context)
-        self.save_inputs(modifier)
+        self._save_inputs(modifier)
 
     def diff(self, modifier: T.bpy_struct, key: Union[int, str], prop: T.Property, context: Context) -> Optional[Delta]:
 
         delta = super().diff(modifier, key, prop, context)
-        inputs = self.load_inputs(modifier)
+        inputs = self._load_inputs(modifier)
         if inputs != self._inputs:
             if delta is None:
                 diff = self.__class__()
@@ -135,5 +145,5 @@ class NodesModifierProxy(StructProxy):
                 ng = modifier.node_group
                 modifier.node_group = None
                 modifier.node_group = ng
-                self.save_inputs(modifier)
+                self._save_inputs(modifier)
         return self
