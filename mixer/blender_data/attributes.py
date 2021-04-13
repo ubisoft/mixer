ubisoft/mixer

commit 7a898d6c496ad5c0779537ce269aae61354bb5ed

fix: add element in bpy_prop_collection_idprop

mixer/blender_data/specifics.py
@@ -552,14 +552,11 @@ def pre_save_datablock(proxy: DatablockProxy, target: T.ID, context: Context) ->
 def add_element(collection: T.bpy_prop_collection, proxy: Proxy, index: int, context: Context):
     """Add an element to a bpy_prop_collection using the collection specific API"""
     try:
-        collection.bl_rna
-    except AttributeError:
-        return
-
-    try:
         collection.add()
-    except Exception:
-        logger.error(f"add_element: failed for {collection}")
+    except Exception as e:
+        logger.error(f"add_element: call to add() failed for {context.visit_state.display_path()} ...")
+        logger.error(f"... {e!r}")
+        raise AddElementFailed from None
 
 
 @add_element.register_default()

commit fe6cd2825da016edc60750bf343a715946b200fb

fix: Pose.bones error caused by 7a898d6c

mixer/blender_data/attributes.py
@@ -92,6 +92,9 @@ def read_attribute(attr: Any, key: Union[int, str], attr_property: T.Property, p
 
                 return AosProxy().load(attr, key, attr_property, context)
             else:
+                # This code path is taken for collections that have an rna and collections that do not
+                # There should probably be different proxies for collection with and without rna.
+                # See comment in add_element()
                 from mixer.blender_data.struct_collection_proxy import StructCollectionProxy
 
                 return StructCollectionProxy.make(attr_property).load(attr, key, attr_property, context)

mixer/blender_data/specifics.py
@@ -550,13 +550,29 @@ def pre_save_datablock(proxy: DatablockProxy, target: T.ID, context: Context) ->
 #
 @dispatch_rna
 def add_element(collection: T.bpy_prop_collection, proxy: Proxy, index: int, context: Context):
-    """Add an element to a bpy_prop_collection using the collection specific API"""
-    try:
-        collection.add()
-    except Exception as e:
-        logger.error(f"add_element: call to add() failed for {context.visit_state.display_path()} ...")
-        logger.error(f"... {e!r}")
-        raise AddElementFailed from None
+    """Add an element to a bpy_prop_collection using the collection specific API.s"""
+
+    if hasattr(collection, "add"):
+        # either a bpy_prop_collection  with an rna or a bpy_prop_collection_idprop
+        try:
+            collection.add()
+            return
+        except Exception as e:
+            logger.error(f"add_element: call to add() failed for {context.visit_state.display_path()} ...")
+            logger.error(f"... {e!r}")
+            raise AddElementFailed from None
+
+    if not hasattr(collection, "bl_rna"):
+        # a bpy.types.bpy_prop_collection, e.g Pose.bones
+        # We should not even attempt to add elements in these collections since they do not allow it at all.
+        # However bpy_prop_collection and collections with an rna both managed by StructCollectionProxy. We need
+        # proxy update to update the contents of existing elements, but it should not attempt to add/remove elements.
+        # As a consequence, for attributes that fall into this category we trigger updates with additions and
+        # deletions that are meaningless. Ignore them.
+        # The right design could be to have different proxies for bpy_prop_collection and bpy_struct that behave like
+        # collections.
+        # see Proxy construction in  read_attribute()
+        return
 
 
 @add_element.register_default()