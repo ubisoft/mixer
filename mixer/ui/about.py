cad03a1a3171160666bb54e6310c4305a9d588b0
Philippe Crassous <philippe.crassous@ubisoft.com>
2021-04-20 11:35:08 +0200

    fix: read about date from release date

--- mixer/ui/about.py
+++ mixer/ui/about.py
@@ -19,10 +19,12 @@
 This module defines the About panel.
 """
 
+import time
+
 import bpy
 from bpy.types import Operator
 
-from mixer import display_version
+from mixer import display_version, about_date
 
 
 class Mixer_OT_About(Operator):  # noqa 801
@@ -43,7 +45,7 @@ class Mixer_OT_About(Operator):  # noqa 801
         row = box.row()
         row.separator()
         row.label(
-            text=f"Version: {display_version or '(Unknown version)'}   -   ({'March 2021'})   -    Ubisoft Animation Studio"
+            text=f"Version: {display_version or '(Unknown version)'}   -   {about_date()}   -    Ubisoft Animation Studio"
         )
 
         # Authors
